4b28479843c8c34f92f48d5d81315dc6d8227572
seyed atheeq <113854491+atheeqgit@users.noreply.github.com>
2023-11-15 23:19:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,13 @@ const { readdirSync } = require("fs");
 
 //creating express app and cors
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://expense-tracker-api-three.vercel.app"],
+    methods: ["POST", "GET", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
